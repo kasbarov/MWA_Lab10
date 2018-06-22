8758bc93d745265f851c94ade940cfcdd7a9e37f
Khalid Dakeen <kh.dakeen@afromoney-ltd.com>
2018-06-22 01:39:10 -0500

    ES code to type script code

---
+++ Classes.ts
@@ -0,0 +1,24 @@
+class Car {
+
+    acceleration: number = 0
+
+    constructor(public name: string) {
+
+    }
+
+
+
+    honk() {
+        console.log(`${this.name} is saying: Tooooooooooot!`);
+    }
+
+    accelerate(speed: number) {
+        this.acceleration += speed;
+    }
+}
+
+const car = new Car('BMW');
+car.honk();
+console.log(car.acceleration);
+car.accelerate(60);
+console.log(car.acceleration);
